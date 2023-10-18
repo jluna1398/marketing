4284f297523c27c10cf5e9603953cc04a6d5f209
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-17 21:01:46 -0700

    new filepsdsfsdfgdsfsdfsdfdfsdf

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,7 +1,24 @@
+import pandas as pd
 import streamlit as st
+import seaborn as sns
 
 st.title(
     "Clustering Analysis"
 
 )
 st.image('images/c.png')
+
+mall_data = pd.read_csv('data/Mall_Customers.csv')
+mall_data.rename({'Annual Income (k$)':'Income', \
+              'Spending Score (1-100)':'Spend_score'}, axis=1, \
+             inplace=True)
+
+st.write("""
+
+#### Data Exploration
+""")
+
+st.dataframe(mall_data.head())
+
+a = sns.histplot(mall_data["Income"])
+st.pyplot(a)
