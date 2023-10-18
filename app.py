e32cd0bedaeb48449a207ea5cd79b63fb7c9799d
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-17 20:43:55 -0700

    new filepsdsfsdfg

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ sns.set_theme(style="darkgrid")
 
 
 def main_page():
-    st.markdown("Data Science for Marketing")
+    st.title("Data Science for Marketing")
 
 
 def page2():
@@ -21,11 +21,3 @@ def page3():
     st.markdown("# Page 3 🎉")
     st.sidebar.markdown("# Page 3 🎉")
 
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
-}
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
