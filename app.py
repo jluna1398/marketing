5a3cc5bd54dfa5677ff26222b8f7f9d5aeb9b3ee
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-17 20:17:26 -0700

    new filep

--- app.py
+++ app.py
@@ -3,3 +3,15 @@ import seaborn as sns
 import sklearn as sk
 import numpy as np
 st.title("Jose Luna")
+import seaborn as sns
+sns.set_theme(style="darkgrid")
+
+# Load an example dataset with long-form data
+fmri = sns.load_dataset("fmri")
+
+# Plot the responses for different events and regions
+a= sns.lineplot(x="timepoint", y="signal",
+             hue="region", style="event",
+             data=fmri)
+
+st.pyplot(a)
