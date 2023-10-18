4741e3183a4c3214cc5277541f32fe7020afa908
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-17 20:28:07 -0700

    new filep

--- app.py
+++ app.py
@@ -7,5 +7,26 @@ import seaborn as sns
 sns.set_theme(style="darkgrid")
 
 # Load an example dataset with long-form da
-st.title("Data Science For Marketing")
-st.image('images/mk.png')
+
+
+
+def main_page():
+    st.title("Data Science For Marketing")
+    st.image('images/mk.png')
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
