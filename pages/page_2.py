59e360da1c4739ed5a2e32a57bdee8ccdfb0222e
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-18 21:12:08 -0700

    new filepsdsfsdfgdsfsdfsdfdfsdf

--- pages/page_2.py
+++ pages/page_2.py
@@ -8,7 +8,7 @@ st.title(
 )
 # st.image('images/c.png')
 
-mall_data = pd.read_csv('../data/Mall_Customers.csv')
+mall_data = pd.read_csv('data/Mall_Customers.csv')
 mall_data.rename({'Annual Income (k$)':'Income', \
               'Spending Score (1-100)':'Spend_score'}, axis=1, \
              inplace=True)
