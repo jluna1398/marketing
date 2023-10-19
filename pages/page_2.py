ee6bf3b8a5fc320fc779d7fa3902c54c27ef4328
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-18 21:14:41 -0700

    new filepsdsfsdfgdsfsdfsdfdfsdf

--- pages/page_2.py
+++ pages/page_2.py
@@ -8,21 +8,21 @@ st.title(
 )
 # st.image('images/c.png')
 
-mall_data = pd.read_csv('data/Mall_Customers.csv')
-mall_data.rename({'Annual Income (k$)':'Income', \
-              'Spending Score (1-100)':'Spend_score'}, axis=1, \
-             inplace=True)
+# mall_data = pd.read_csv('data/Mall_Customers.csv')
+# mall_data.rename({'Annual Income (k$)':'Income', \
+#               'Spending Score (1-100)':'Spend_score'}, axis=1, \
+#              inplace=True)
+#
+# st.write(mall_data.head())
+#
+#
+#
+# a = alt.Chart(mall_data).mark_bar().encode(
+#     alt.X("Income", bin=True),
+#     y='count()'
+# )
 
-st.write(mall_data.head())
-
-
-
-a = alt.Chart(mall_data).mark_bar().encode(
-    alt.X("Income", bin=True),
-    y='count()'
-)
-
-st.altair_chart(a, use_container_width=True)
+# st.altair_chart(a, use_container_width=True)
 
 import plotly.express as px
 
