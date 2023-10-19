395dffe8eba22cfcbb21ff35e5474108a8b6717a
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-18 21:11:22 -0700

    new filepsdsfsdfgdsfsdfsdfdfsdf

--- pages/page_2.py
+++ pages/page_2.py
@@ -6,12 +6,12 @@ st.title(
     "Clustering Analysis"
 
 )
-st.image('images/c.png')
+# st.image('images/c.png')
 
-mall_data = pd.read_csv('data/Mall_Customers.csv')
+mall_data = pd.read_csv('../data/Mall_Customers.csv')
 mall_data.rename({'Annual Income (k$)':'Income', \
               'Spending Score (1-100)':'Spend_score'}, axis=1, \
-             inplace=True).set_index("CustomerID")
+             inplace=True)
 
 st.write(mall_data.head())
 
@@ -23,3 +23,17 @@ a = alt.Chart(mall_data).mark_bar().encode(
 )
 
 st.altair_chart(a, use_container_width=True)
+
+import plotly.express as px
+
+df = pd.DataFrame(dict(
+    r=[1, 5, 2, 2, 3],
+    theta=['processing cost','mechanical properties','chemical stability',
+           'thermal stability', 'device integration']))
+fig = px.line_polar(df, r='r', theta='theta', line_close=True)
+fig.show()
+
+
+# we can divided the gruops into 3 categories. By using age
+
+# hypotesis is a therory of explanmation based on evidence that is not yet proved to be true
