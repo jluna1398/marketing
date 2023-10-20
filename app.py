baf5f87fedd50f61ad0e165446ea16d4de8acbd0
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-19 17:23:04 -0700

    new updated file addedass

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ page_names_to_funcs = {
     "Page 3": page3,
 }
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+# selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+# page_names_to_funcs[selected_page]()
